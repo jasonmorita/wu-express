2d4d5d066870c27d83b4d1b161919280c31f0c19
Jason Morita <jason@blst.com>
2016-02-28 18:23:23 -0800

    async working - needs minor refactor

---
+++ middleware/getDefaults.js
@@ -0,0 +1,41 @@
+var async = require('async');
+var request = require('request');
+var config = require('../config.js');
+
+module.exports = function(req, res, next) {
+
+    // hardcoded cities to be used in example
+    var defaultLocations = [
+    	{state: 'CA', city: 'Campbell'},
+    	{state: 'MD', city: 'Timonium'},
+    	{state: 'NE', city: 'Omaha'},
+    	{state: 'TX', city: 'Austin'}
+    ];
+
+    var urls = defaultLocations.map(function(location) {
+        return 'http://api.wunderground.com/api/' + config.apiKey + '/conditions/q/' + location.state + '/' + location.city + '.json';
+    });
+
+    var results = [];
+
+    // iterator to be called on each element in url array
+    function httpGet(url, callback) {
+        // make the call
+        request(url, function(err, res, body) {
+                results.push(JSON.parse(body));
+                callback(err, body);
+            }
+        );
+    }
+
+    // map over urls to get location info
+    async.map(urls, httpGet, function (err, response){
+        if (err) {
+            return console.log(err);
+        }
+
+        // this will get passed on to be used in view
+        res.results = results;
+        next();
+    });
+}
--- server.js
+++ server.js
@@ -1,36 +1,31 @@
 var express = require('express');
 var helmet = require('helmet');
 var hbs = require('hbs');
+var _ = require('lodash');
+var engine = require('hbs').__express;
+var getDefaults = require('./middleware/getDefaults');
+var config = require('./config.js');
 var paramLogger = require('./middleware/paramLogger');
 var jsonOut = require('./middleware/jsonOut');
 
 var app = express();
 var router = express.Router();
 
-// set some default security
-app.use(helmet());
-
 // set up handlebars to be view engine
-app.engine('hbs', require('hbs').__express);
+app.engine('hbs', engine);
 app.set('view engine', 'hbs');
 app.set('views', './views');
 hbs.registerPartials(__dirname + '/views/partials');
 
-// hardcoded cities to be used in example
-var defaultLocations = [
-	{state: 'CA', city: 'Campbell'},
-	{state: 'MD', city: 'Timonium'},
-	{state: 'NE', city: 'Omaha'},
-	{state: 'TX', city: 'Austin'},
-	{state: '', city: 'Nowhere'},
-	{state: 'X', city: 'X'}
-];
+// set some default security
+app.use(helmet());
 
-// root route.
-app.get('/', function(req, res) {
+// root route
+app.route('/').get(getDefaults, function(req, res) {
+    var results = _.uniq(res.results);
 	res.render('index', {
 		title: "Choose Weather Report",
-		locations: defaultLocations
+		results: results
 	});
 });
 
