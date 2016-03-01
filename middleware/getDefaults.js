b6b7e29fd04d82e84c64d22bb880e59413d69825
Jason Morita <jason@blst.com>
2016-02-29 19:53:55 -0800

    minor refactor of defaults middleware to be cleaner

--- middleware/getDefaults.js
+++ middleware/getDefaults.js
@@ -6,36 +6,37 @@ module.exports = function(req, res, next) {
 
     // hardcoded cities to be used in example
     var defaultLocations = [
-    	{state: 'CA', city: 'Campbell'},
-    	{state: 'MD', city: 'Timonium'},
-    	{state: 'NE', city: 'Omaha'},
-    	{state: 'TX', city: 'Austin'}
+        {state: 'CA', city: 'Campbell'},
+        {state: 'MD', city: 'Timonium'},
+        {state: 'NE', city: 'Omaha'},
+        {state: 'TX', city: 'Austin'}
     ];
 
     var urls = defaultLocations.map(function(location) {
         return 'http://api.wunderground.com/api/' + config.apiKey + '/conditions/q/' + location.state + '/' + location.city + '.json';
     });
 
-    var results = [];
-
     // iterator to be called on each element in url array
     function getData(url, callback) {
         // make the call
         request(url, function(err, res, body) {
-                results.push(JSON.parse(body));
-                callback(err, body);
-            }
-        );
+            callback(err, body);
+        });
     }
 
     // map over urls to get location info
-    async.map(urls, getData, function (err, response){
+    async.map(urls, getData, function (err, results){
         if (err) {
             return console.log(err);
         }
 
+        // make sure results are proper JSON
+        var json = results.map(function(value) {
+            return JSON.parse(value);
+        })
+
         // this will get passed on to be used in view
-        req.results = results;
+        req.results = json;
         next();
     });
 }
