0d2e36211c0d67dbf5c932e3f7447b05791df5ec
Jason Morita <jason@blst.com>
2016-02-29 19:54:57 -0800

    fixing indents

--- middleware/jsonOut.js
+++ middleware/jsonOut.js
@@ -2,17 +2,17 @@ var request = require('request');
 var config = require('../config.json');
 
 module.exports = function jsonOut(req, res, next) {
-	// wu api path
-	var url = 'http://api.wunderground.com/api/' + config.apiKey + '/conditions/q/' + req.params.state + '/' + req.params.city + '.json';
+    // wu api path
+    var url = 'http://api.wunderground.com/api/' + config.apiKey + '/conditions/q/' + req.params.state + '/' + req.params.city + '.json';
 
-	request(url, function(err, response, body) {
+    request(url, function(err, response, body) {
         if (err) {
             return console.log(err);
         }
 
-		// good response
-		if (!err && response.statusCode === 200) {
-			res.json(JSON.parse(body));
-		}
-	});
+        // good response
+        if (!err && response.statusCode === 200) {
+            res.json(JSON.parse(body));
+        }
+    });
 };
